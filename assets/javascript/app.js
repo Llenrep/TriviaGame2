db4a2fc4297024954cf36be7176471a56e071504
Llenrep <lppernell1998@gmai.com>
2018-11-19 21:53:02 -0500

    okayyyyy sooooo its done/ ill attempt to add giphy stuff

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -12,82 +12,42 @@ var running = true;
 
 function giphyInsert() { //we fill it in later when we get the first question to work
 
-}
-
-
-function question1() {
-    console.log("Question 1");
-
-};
-
-function question2() {
-    console.log("Question 2");
 
 }
 
-function question3() {
-    console.log("Question 3");
-
-}
-
-function question4() {
-    console.log("Question 4");
-
-}
-
-function question5() {
-    console.log("Question 5");
-
-}
-
-function question6() {
-    console.log("Question 6");
-
-}
-
-function question7() {
-    console.log("Question 7");
-
-}
-
-function question8() {
-    console.log("Question 8");
-
-}
 
 function timer() {
-    
-    if (running === true){
-    var the_timer = setTimeout(function () {
 
-        time--; //its a countdown so we are looking for decerements specifically
+    if (running === true) {
+        var the_timer = setTimeout(function () {
 
-        var secs = (time)
+            time--; //its a countdown so we are looking for decerements specifically
 
-        document.getElementById("timer").innerHTML = "Time Left: " + secs
+            var secs = (time)
 
-        timer();
-        
-        if (secs === 0) {
+            document.getElementById("timer").innerHTML = "Time Left: " + secs
 
-            running = false;
+            timer();
 
-            time = 100;
+            if (secs === 0) {
 
-            alert("Times up!");
+                running = false;
 
-            // clearTimeout(timer(), 100000)
-        };
-        
-    }, 1000);
+                time = 100;
+
+                alert("Times up!");
+
+                // clearTimeout(timer(), 100000)
+            };
+
+        }, 1000);
     }
 }
 
 function result() { //just formats the page to show a "results" screen
     running = false;
-    console.log("Result")
-    $("#buttons").html("");
-    $(".timer").html(""); 
+    $("#buttons").empty(); //no need for buttons?
+    $("#timer").empty(); //empties that time thing within the results div, unecessary
 
     $(".Correct").text("Your amount correct: " + amountCorrect);
     $(".Wrong").text("Your amount wrong: " + amountWrong);
@@ -97,39 +57,135 @@ function result() { //just formats the page to show a "results" screen
 
 $(document).ready(function () { // where are the functions will be run with specific rules and order.... I guess....
     
-        var type1 = document.getElementsByName("inlineRadio1Options");
-        console.log(type1[0])
-        var type2 = document.getElementsByName("inlineRadio2Options");
-        console.log(type2[1])
-        var type3 = document.getElementsByName("inlineRadio3Options");
-        console.log(type3[0])
-        var type4 = document.getElementsByName("inlineRadio4Options");
-        console.log(type4[2])
-        var type5 = document.getElementsByName("inlineRadio5Options");
-        console.log(type5[3])
-        var type6 = document.getElementsByName("inlineRadio6Options");
-        console.log(type6[1])
-        var type7 = document.getElementsByName("inlineRadio7Options");
-        console.log(type7[3])
-        var type8 = document.getElementsByName("inlineRadio8Options");
-        console.log(type8[1])
-        
+    var type1 = document.getElementsByName("inlineRadio1Options");
+    // console.log(type1[0])
+    var type2 = document.getElementsByName("inlineRadio2Options");
+    // console.log(type2[1])
+    var type3 = document.getElementsByName("inlineRadio3Options");
+    // console.log(type3[0])
+    var type4 = document.getElementsByName("inlineRadio4Options");
+    // console.log(type4[2])
+    var type5 = document.getElementsByName("inlineRadio5Options");
+    // console.log(type5[3])
+    var type6 = document.getElementsByName("inlineRadio6Options");
+    // console.log(type6[1])
+    var type7 = document.getElementsByName("inlineRadio7Options");
+    // console.log(type7[3])
+    var type8 = document.getElementsByName("inlineRadio8Options");
+    // console.log(type8[1])
     
-
     timer();
 
     if (running === false) {
+        
+        if (type1[0].checked) {
+            amountCorrect++;
+        } else {
+            amountWrong++;
+        }
+    
+        if (type2[1].checked) {
+            amountCorrect++;
+        } else {
+            amountWrong++;
+        }
+        
+        if (type3[0].checked) {
+            amountCorrect++;    
+        } else {
+            amountWrong++;
+        }
+    
+        if (type4[2].checked) {
+            amountCorrect++;
+        } else {
+            amountWrong++;
+        }
+    
+        if (type5[3].checked) {
+            amountCorrect++;
+        } else {
+            amountWrong++;
+        }
+    
+        if (type6[1].checked) {
+            amountCorrect++;
+        } else {
+            amountWrong++;
+        }
+    
+        if (type7[3].checked) {
+            amountCorrect++;
+        } else {
+            amountWrong++;
+        }
+    
+        if (type8[1].checked) {
+            amountCorrect++;
+        } else {
+            amountWrong++;
+        }
         result();
         $("#actualQuestions").empty()
         $("#timer").empty()
     };
 
-    $("#finished").on("click", function(){
+    $("#finished").on("click", function () {
+        event.preventDefault();
+
+        if (type1[0].checked) {
+            amountCorrect++;
+        } else {
+            amountWrong++;
+        }
+    
+        if (type2[1].checked) {
+            amountCorrect++;
+        } else {
+            amountWrong++;
+        }
+        
+        if (type3[0].checked) {
+            amountCorrect++;    
+        } else {
+            amountWrong++;
+        }
+    
+        if (type4[2].checked) {
+            amountCorrect++;
+        } else {
+            amountWrong++;
+        }
+    
+        if (type5[3].checked) {
+            amountCorrect++;
+        } else {
+            amountWrong++;
+        }
+    
+        if (type6[1].checked) {
+            amountCorrect++;
+        } else {
+            amountWrong++;
+        }
+    
+        if (type7[3].checked) {
+            amountCorrect++;
+        } else {
+            amountWrong++;
+        }
+    
+        if (type8[1].checked) {
+            amountCorrect++;
+        } else {
+            amountWrong++;
+        }
+
         running = false;
         result();
         $("#actualQuestions").empty()
         $("#timer").empty()
-    
+
     })
 
 }) 
